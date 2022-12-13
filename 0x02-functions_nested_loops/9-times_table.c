ba6c39f78f13305bbb94bbf813277734b5a424d7
molistephoguta <molistephoguta@gmail.com>
2022-12-13 13:52:27 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,37 +2,38 @@
 
 /**
  * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
  * Return: times table
  */
 void times_table(void)
 {
-	int num1, mum2, prod;
+	int rone, cone, d;
 
-	for (num = 0; num <= 9; num++)
+	for (rone = 0; rone <= 9; rone++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (num2 = 1; num2 <= 9; num2++)
+		for (cone = 1; cone <= 9; cone++)
 		{
-			prod = (num1 * num2);
-			if ((prod / 10) > 0)
+			d = (rone * cone);
+			if ((d / 10) > 0)
 			{
-				_putchar((prod / 10) + '0');
+				_putchar((d / 10) + '0');
 			}
 			else
 			{
 				_putchar(' ');
 			}
-			_putchar((prod % 10) + '0');
+			_putchar((d % 10) + '0');
 
 			if (cone < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
+
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
-
