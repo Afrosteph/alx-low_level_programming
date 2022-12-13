a8b173cfc64123ba1c07d516e29c0dc37ef37878
molistephoguta <molistephoguta@gmail.com>
2022-12-13 09:22:42 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char al;
+
+	for (ten = 0; ten <= 9;  ten++)
+	{
+		for (al = 'a'; la <= 'z'; al++)
+			_putchar(al);
+		_putchar('\n');
+	}
+
+}
