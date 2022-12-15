89fd43f3fc73304f63b41e8772514a591a802e80
molistephoguta <molistephoguta@gmail.com>
2022-12-15 11:36:07 -0800

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,14 +13,17 @@ if (a > b && b > c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b > c && b > a)
 {
 largest = b;
 }
-else
+else if (c > b)
 {
 largest = c;
 }
-
+else
+}
+largest = b;
+}
 return (largest);
 }
