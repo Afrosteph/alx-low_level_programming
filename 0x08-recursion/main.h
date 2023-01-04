7814a9814301a863e74eb2ce2ed5b6eb59de2d0f
molistephoguta <molistephoguta@gmail.com>
2023-01-04 12:10:25 -0800

    task 7

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
+
