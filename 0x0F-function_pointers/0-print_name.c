4d4ce46eaf95f3c145cc17abfd2aae8d954636c5
molistephoguta <molistephoguta@gmail.com>
2023-01-18 09:04:24 -0800

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 /**
- * print_name - prints the name
- * @name: name
- * @f: function
+ * print_name - prints the name.
+ * @name: pointer to name.
+ * @f: function pointer.
+ *
+ * Return: void.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
